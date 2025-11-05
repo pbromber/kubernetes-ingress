993b5f8d4c48251f4a24e8b99405ae2ef66f3247
Przemek Bromber <pbromber@gmail.com>
2025-11-05 19:33:48 +0100

    generate-certificates and ca-sign-file bind options in https frontend

--- pkg/haproxy/api/frontend.go
+++ pkg/haproxy/api/frontend.go
@@ -73,7 +73,7 @@ func (c *clientNative) FrontendEdit(frontend models.FrontendBase) error {
 	return configuration.EditFrontend(frontend.Name, f, c.activeTransaction, 0)
 }
 
-func (c *clientNative) FrontendEnableSSLOffload(frontendName string, certDir string, alpn string, strictSNI bool) (err error) {
+func (c *clientNative) FrontendEnableSSLOffload(frontendName string, certDir string, alpn string, strictSNI bool, generateCertificates bool, caSignFile string) (err error) {
 	binds, err := c.FrontendBindsGet(frontendName)
 	if err != nil {
 		return err
@@ -81,6 +81,10 @@ func (c *clientNative) FrontendEnableSSLOffload(frontendName string, certDir str
 	for _, bind := range binds {
 		bind.Ssl = true
 		bind.SslCertificate = certDir
+		bind.GenerateCertificates = generateCertificates
+		if caSignFile != "" {
+			bind.CaSignFile = caSignFile
+		}
 		if alpn != "" {
 			bind.Alpn = alpn
 			bind.StrictSni = strictSNI
@@ -103,8 +107,10 @@ func (c *clientNative) FrontendDisableSSLOffload(frontendName string) (err error
 		bind.SslCafile = ""
 		bind.Verify = ""
 		bind.SslCertificate = ""
+		bind.CaSignFile = ""
 		bind.Alpn = ""
 		bind.StrictSni = false
+		bind.GenerateCertificates = false
 		err = c.FrontendBindEdit(frontendName, *bind)
 	}
 	if err != nil {
